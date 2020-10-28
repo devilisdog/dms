28e1aaa912c441e34acec5e6312ff049d67fc130
Dean <>
2020-10-28 15:08:18 +0800

    update gitignore

--- node_modules/@ant-design/pro-table/lib/component/ToolBar/DensityIcon.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export declare type DensitySize = 'middle' | 'small' | 'large' | undefined;
-declare const _default: React.ForwardRefExoticComponent<React.RefAttributes<any>>;
-export default _default;
